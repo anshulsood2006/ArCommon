2c3c2638b8f2c8c4dea1f9984cff07b562534f7d
Anshul Sood <anshulsood2006@gmail.com>
2017-06-28 22:57:52 +0530

    Some code refactoring done

--- src/main/java/com/arsoft/projects/common/string/ArStringUtil.java
+++ src/main/java/com/arsoft/projects/common/string/ArStringUtil.java
@@ -48,47 +48,4 @@ public class ArStringUtil {
         } 
     }
 	
-	public static String readFile(String filePath) throws IOException{
-		InputStream is = new FileInputStream(filePath); 
-		BufferedReader buf = new BufferedReader(new InputStreamReader(is)); 
-		String line = buf.readLine(); 
-		StringBuilder sb = new StringBuilder(); 
-		while(line != null){ 
-			sb.append(line).append("\n"); 
-			line = buf.readLine(); 
-		} 
-		buf.close();
-		String fileAsString = sb.toString(); 
-		return fileAsString;
-	}
-	
-	public static List<File> getFoldersAtPath(String path){
-		List<File> folderList = new ArrayList<>();
-		File file = new File(path);
-		String[] names = file.list();
-		for(String name : names)
-		{
-			String filePath = path + File.separator + name;
-		    if (new File(filePath).isDirectory())
-		    {
-		    	folderList.add(new File(filePath));
-		    }
-		}
-		return folderList;
-	}
-	
-	public static List<File> getFilesAtPath(String path){
-		List<File> fileList = new ArrayList<>();
-		File file = new File(path);
-		String[] names = file.list();
-		for(String name : names)
-		{
-			String filePath = path + File.separator + name;
-		    if (!new File(filePath).isDirectory())
-		    {
-		    	fileList.add(new File(filePath));
-		    }
-		}
-		return fileList;
-	}
 }
