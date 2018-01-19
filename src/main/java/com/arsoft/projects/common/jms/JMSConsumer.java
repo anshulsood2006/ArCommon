b4466d649b041aeefa5946488885acb562cb48d1
Anshul Sood <anshul.sood@globallogic.com>
2018-01-19 16:47:13 +0530

    Added JMS Consumer

--- src/main/java/com/arsoft/projects/common/jms/JMSConsumer.java
+++ src/main/java/com/arsoft/projects/common/jms/JMSConsumer.java
@@ -1,13 +1,61 @@
 package com.arsoft.projects.common.jms;
 
-import javax.jms.Message;
-import javax.jms.MessageListener;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class JMSConsumer implements MessageListener {
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.QueueConnection;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/receive")
+public class JMSConsumer extends HttpServlet {
+
+	private static final long serialVersionUID = -8947771123245679449L;
+
+	@Resource(lookup = JMSConstant.JMS_QUEUE_CONNECTION_FACTORY)
+	ConnectionFactory connectionFactory;
+
+	@Resource(lookup = JMSConstant.JMS_QUEUE_ADDRESS)
+	Destination destination;
+
+	QueueConnection connection = null;
+	QueueSession session = null;
+	TextMessage message = null;
+	MessageConsumer consumer = null;
 
 	@Override
-	public void onMessage(Message message) {
-				
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		try {
+			connection = (QueueConnection) connectionFactory.createConnection(JMSConstant.JMS_USERNAME,
+					JMSConstant.JMS_PASSWORD);
+			session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			consumer = session.createConsumer(destination);
+			connection.start();
+			message = (TextMessage) consumer.receive();
+			writer.println("Message received! " + message.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				consumer.close();
+				session.close();
+				connection.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
