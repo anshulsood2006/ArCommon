cf45f97e9cc9a31373a68b6ac90b77444923db4a
Anshul Sood <anshul.sood@globallogic.com>
2018-01-25 15:32:12 +0530

    Data for 25/1/2018

--- src/main/java/com/arsoft/projects/common/controller/ArCommonBaseController.java
+++ src/main/java/com/arsoft/projects/common/controller/ArCommonBaseController.java
@@ -1,26 +1,19 @@
 package com.arsoft.projects.common.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class ArCommonBaseController {
 	
 	private static final String VIEW_INDEX = "index";
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
-
-		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return VIEW_INDEX;
 
 	}
-	
-	@RequestMapping(value = "/arcommon-1.0/", method = RequestMethod.GET)
-	public String welcomeOne(ModelMap model) {
 
-		// Spring uses InternalResourceViewResolver and return back index.jsp
-		return VIEW_INDEX;
-
-	}
 }
