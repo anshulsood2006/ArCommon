0df1ee47b5536d927d5f852c3fcc16c0c42b55c5
Anshul Sood <anshulsood2006@gmail.com>
2018-02-13 23:15:24 +0530

    Updated the wishes code

--- src/main/java/com/arsoft/projects/common/servlet/ArWisher.java
+++ src/main/java/com/arsoft/projects/common/servlet/ArWisher.java
@@ -16,26 +16,31 @@ public class ArWisher extends HttpServlet {
 
 	private static final long serialVersionUID = 6504618863196061212L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String to = request.getParameter("to");
-		response.setContentType("text/html");// setting the content type
-		PrintWriter pw = response.getWriter();// get the stream to write the	
-		URL u = new URL("http://localhost:8080/wish.html");
-		InputStream ins = u.openStream();
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
-			if (ins != null) {
-				InputStreamReader isr = new InputStreamReader(ins);
-				BufferedReader reader = new BufferedReader(isr);
-				String word = "";
-				while ((word = reader.readLine()) != null) {
-					if (word.indexOf("$$") > 1) {
-						word = word.replace("$$", to+" wishes you and your family");
+			String to = request.getParameter("from");
+			response.setContentType("text/html");// setting the content type
+			PrintWriter pw = response.getWriter();// get the stream to write the	
+			URL u = new URL("http://myfriendwa.com/wish.html");
+			InputStream ins = u.openStream();
+			try {
+				if (ins != null) {
+					InputStreamReader isr = new InputStreamReader(ins);
+					BufferedReader reader = new BufferedReader(isr);
+					String word = "";
+					while ((word = reader.readLine()) != null) {
+						if (word.indexOf("$$") > 1) {
+							word = word.replace("$$", to+" wishes you and your family");
+						}
+						pw.println(word);
 					}
-					pw.println(word);
 				}
+			pw.write("<center><a href=\"whatsapp://send?text=\" onClick=\"sendViaWatsapp(); return false;\" class=\"button\">Share via watsapp</a></center>");
+			} finally {
+				pw.close();
 			}
-		} finally {
-			pw.close();
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
