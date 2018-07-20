b141db9758129c2cddbdc5c2e0ed279d0c431c90
Anshul Sood <anshul.sood@globallogic.com>
2018-07-20 16:48:41 +0530

    Added Atomic in learning

---
+++ src/main/java/com/arsoft/projects/common/learning/multithreading/AlternatePrint.java
@@ -0,0 +1,93 @@
+package com.arsoft.projects.common.learning.multithreading;
+
+public class AlternatePrint {
+	
+	class MyThreadOne implements Runnable{
+		private ArLock arLock;
+		public MyThreadOne(ArLock arLock) {
+			this.arLock = arLock;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i <10; i++){
+				synchronized(this.arLock){
+					if (this.arLock.flag == 1){
+						System.out.println("1");
+						this.arLock.flag = 2;
+						try {
+							arLock.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					arLock.notify();
+				}
+			}
+		}
+		
+	}
+	
+	
+	class MyThreadTwo implements Runnable{
+		private ArLock arLock;
+		public MyThreadTwo(ArLock arLock) {
+			this.arLock = arLock;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i <10; i++){
+				synchronized(this.arLock){
+					if (this.arLock.flag == 2){
+						System.out.println("2");
+						this.arLock.flag = 3;
+						arLock.notify();
+					}
+					try {
+						arLock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}			
+		}
+	}
+	
+	class MyThreadThree implements Runnable{
+		private ArLock arLock;
+		public MyThreadThree(ArLock arLock) {
+			this.arLock = arLock;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i <10; i++){
+				synchronized(this.arLock){
+					if (this.arLock.flag == 3){
+						System.out.println("3");
+						this.arLock.flag = 1;
+						arLock.notify();
+					}
+					try {
+						arLock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}			
+		}
+	}
+	
+	public static void main(String[] args) {
+		AlternatePrint ap = new AlternatePrint();
+		ArLock arLock = new ArLock();
+		Thread t1 = new Thread (ap.new MyThreadOne(arLock));
+		Thread t2 = new Thread (ap.new MyThreadTwo(arLock));
+		Thread t3 = new Thread (ap.new MyThreadThree(arLock));
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
--- src/main/java/com/arsoft/projects/common/learning/multithreading/TestSynchronization1.java
+++ src/main/java/com/arsoft/projects/common/learning/multithreading/TestSynchronization1.java
@@ -36,7 +36,7 @@ class MyThread2 extends Thread {
 
 class TestSynchronization1 {
 	public static void main(String args[]) {
-		Table obj = new Table();// only one object
+		Table obj = new Table();
 		MyThread1 t1 = new MyThread1(obj);
 		MyThread2 t2 = new MyThread2(obj);
 		t1.start();
