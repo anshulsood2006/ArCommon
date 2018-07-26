6a066a70098851e37d62103834ed5f2288e17a33
Anshul Sood <anshul.sood@globallogic.com>
2018-07-26 16:49:30 +0530

    Added the new files for queue interface

---
+++ src/main/java/com/arsoft/projects/common/learning/collection/queue/arrayblockingqueue/MyConsumer.java
@@ -0,0 +1,25 @@
+package com.arsoft.projects.common.learning.collection.queue.arrayblockingqueue;
+
+import java.util.concurrent.BlockingQueue;
+
+public class MyConsumer implements Runnable{
+	private BlockingQueue queue;
+	
+	public MyConsumer(BlockingQueue queue){
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		while(queue.remainingCapacity() > 0){
+			System.out.println("Queue size: "+queue.size() +", Remaining capacity: "+queue.remainingCapacity());
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+}
---
+++ src/main/java/com/arsoft/projects/common/learning/collection/queue/arrayblockingqueue/MyProducer.java
@@ -0,0 +1,23 @@
+package com.arsoft.projects.common.learning.collection.queue.arrayblockingqueue;
+
+import java.util.concurrent.BlockingQueue;
+
+public class MyProducer implements Runnable{
+	private BlockingQueue queue;
+	
+	public MyProducer(BlockingQueue queue){
+		this.queue = queue;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i <8; i++){	
+			System.out.println("Trying to add to the queue: "+ i +" and the result was "+queue.offer(i));
+			try{
+				Thread.sleep(500);
+			}catch(InterruptedException  e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
