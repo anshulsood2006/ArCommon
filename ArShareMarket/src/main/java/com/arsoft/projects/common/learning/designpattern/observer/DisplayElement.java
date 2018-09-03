e85c512c307ea6fe54fe54501ec93336fdf89e32
Anshul Sood <anshulsood2006@gmail.com>
2018-09-03 21:58:04 +0530

    Observer Pattern

---
+++ ArShareMarket/src/main/java/com/arsoft/projects/common/learning/designpattern/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.arsoft.projects.common.learning.designpattern.observer;
+
+public interface DisplayElement {
+	public void display();
+}
---
+++ ArShareMarket/src/main/java/com/arsoft/projects/common/learning/designpattern/observer/Observer.java
@@ -0,0 +1,5 @@
+package com.arsoft.projects.common.learning.designpattern.observer;
+
+public interface Observer {
+	public void update(float temp, float humidity, float pressure);
+}
