88f23465c9f429f6b2ccfc480d00a7ed129bbc9f
Anshul Sood <anshulsood2006@gmail.com>
2018-11-19 23:23:03 +0530

    Python learning

---
+++ ArTutorial/python/HelloWorld.py
@@ -0,0 +1,40 @@
+print("Hello, World!")
+if 5>2:
+	print("Five is greater than two!")
+#This is a comment
+"""This is just a documentation"""
+x=51
+print(type(x))
+y=21
+print("The sum of x and y is")
+print(y + x)
+x="Anshul Sood"
+print(x)
+print("The value of x is: "+x)
+print(type(x))
+j=12
+k=7.8
+l=1j
+m=-3456
+print(l)
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+k=complex(12)
+print(k)
+z=str("Zakir Hussain")
+print(z[10])
+print(z[1:10])
+z=str(" Zakir Hussain   ")
+print(z.strip())
+print(len(z.strip()))
+print(z.replace("aki","ube"))
+z="ANshUL SooD"
+print(z.lower())
+print(z.upper())
+z="Anshul , Sood,is, A , good"
+print(z.split(","))
+print("What is your name")
+z=input()
+print(z)
--- ArTutorial/src/main/webapp/public/pages/learning/NewTable.jsp
+++ ArTutorial/src/main/webapp/public/pages/learning/NewTable.jsp
@@ -1,4 +1,4 @@
-<table>
+<table border="1">
 	<tr><th></th><th></th><th></th></tr>
 	<tr><td></td><td></td><td></td></tr>
 	<tr><td></td><td></td><td></td></tr>
