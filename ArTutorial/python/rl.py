9e9a7c9a24c2d14f7ddfce6d953cb4a607440bf4
Anshul Sood <anshul.sood@globallogic.com>
2018-11-29 16:07:43 +0530

    ML updated

--- ArTutorial/python/rl.py
+++ ArTutorial/python/rl.py
@@ -2,4 +2,8 @@ import gym
 import RLEnvironment.envs.myenvironment
 env = gym.make('myenv-v0')
 env.reset()
-#env.step(action)
+env = gym.make("Taxi-v2")
+env.reset()
+env.render()
+print("Action Space {}".format(env.action_space))
+print("State Space {}".format(env.observation_space))
