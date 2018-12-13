c9d1e526b4a77a6f8d0753af18ee559b9fcd1900
Anshul Sood <anshul.sood@globallogic.com>
2018-12-13 14:30:05 +0530

    Threadlocal tutorial

--- ArTutorial/src/main/java/com/arsoft/projects/artutorial/learning/multithreading/threadlocal/ArThreadLocal.java
+++ ArTutorial/src/main/java/com/arsoft/projects/artutorial/learning/multithreading/threadlocal/ArThreadLocal.java
@@ -1,50 +1,51 @@
 package com.arsoft.projects.artutorial.learning.multithreading.threadlocal;
 
-import java.text.SimpleDateFormat;
-
 public class ArThreadLocal {
     
-    int var1 = 23;
-    
-    static int var2 = 34;
-    
-    ThreadLocal<SimpleDateFormat> var3 = new ThreadLocal<SimpleDateFormat>() {
-    	@Override
-        protected SimpleDateFormat initialValue()
-        {
-            return new SimpleDateFormat("yyyyMMdd HHmm");
-        }
-    };
+    ThreadLocal<Employee> var1 = new ThreadLocal<Employee>();
+    Employee var2 = new Employee("Akhil", 29);
     
     private class MyThread implements Runnable{
         String name;
-        int var1;
-        
-        MyThread(String name, int var1){ 
+        MyThread(String name){ 
              this.name = name;
-             this.var1 = var1;
         }
-
         @Override
         public void run()
         {
-            System.out.println(this.name + " this.var1: "+this.var1);   
-            System.out.println(this.name + " var1: "+var1);  
-            System.out.println(this.name + " var2: "+var2);
-            System.out.println(this.name + " var3: "+var3.get());
+            var1.set(new Employee("Anshul", 12));
+            System.out.println(this.name + ": Local Varible: "+var2);
+            System.out.println(this.name + ": Thread Local Variable: "+var1.get());
         }
-        
+    }
+    
+    private class Employee{
+        String name;
+        int id;
+        public Employee(String name, int id){
+            this.name = name;
+            this.id = id;
+        }
     }
     
     public static void main(String[] args)
     {
         ArThreadLocal test = new ArThreadLocal();
-        Thread t1 = new Thread(test.new MyThread("T1",13));
-        Thread t2 = new Thread(test.new MyThread("T2", 26));
-        Thread t3 = new Thread(test.new MyThread("T3", 27));
-        t1.start();
-        t2.start();
-        t3.start();
-    }
-    
+        Thread t1 = new Thread(test.new MyThread("T1"));
+        Thread t2 = new Thread(test.new MyThread("T2"));
+        Thread t3 = new Thread(test.new MyThread("T3"));
+        try
+        {
+            t1.start();
+            t1.join();
+            t2.start();
+            t2.join();
+            t3.join();
+            t3.start();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    } 
 }
