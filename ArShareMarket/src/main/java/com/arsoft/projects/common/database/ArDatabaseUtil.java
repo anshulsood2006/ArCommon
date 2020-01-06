ca8673fa53cdc51112e7d24bdbe4077d622ca4db
Anshul Sood <anshulsood2006@gmail.com>
2020-01-06 17:32:46 +0530

    Fixed linux deployment

--- ArShareMarket/src/main/java/com/arsoft/projects/common/database/ArDatabaseUtil.java
+++ ArShareMarket/src/main/java/com/arsoft/projects/common/database/ArDatabaseUtil.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.internal.util.config.ConfigurationException;
+import org.hibernate.service.ServiceRegistry;
 
 import com.arsoft.projects.arshared.exception.ArException;
 
@@ -39,17 +41,22 @@ public class ArDatabaseUtil {
 
 		private static SessionFactory buildSessionFactory(String resource) throws ArException {
 			Configuration configuration = null;
+			ServiceRegistry serviceRegistry = null;
 			if (resource == null) {
 				String exceptionMessage = "Resource can not be null";
 				throw new ArException(exceptionMessage, logger);
 			}
 			 try {
 				configuration = new Configuration().configure(resource);
+	        	serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+	        	if (serviceRegistry == null) {
+					String exceptionMessage = "serviceRegistry can not be null";
+					throw new ArException(exceptionMessage, logger);
+				}
 			 }catch (ConfigurationException e) {
 				throw new ArException("Resource "+resource+" not found on the classpath");
 			}
-			 
-			return configuration.buildSessionFactory();
+			return configuration.buildSessionFactory(serviceRegistry);
 		}
 
 	public static void shutdown(String resource) throws ArException {
