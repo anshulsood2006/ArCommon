f9087e2a76ac98466a0bcb807835f8b5d51c345a
Anshul Sood <anshulsood2006@gmail.com>
2020-01-10 22:49:48 +0530

    Changed database to mysql

--- ArShareMarket/src/main/java/com/arsoft/projects/common/database/ArDatabaseUtil.java
+++ ArShareMarket/src/main/java/com/arsoft/projects/common/database/ArDatabaseUtil.java
@@ -6,10 +6,8 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.internal.util.config.ConfigurationException;
-import org.hibernate.service.ServiceRegistry;
 
 import com.arsoft.projects.arshared.exception.ArException;
 
@@ -39,25 +37,23 @@ public class ArDatabaseUtil {
 		return mapSessionFactory.get(resource);
 	}
 
-		private static SessionFactory buildSessionFactory(String resource) throws ArException {
-			Configuration configuration = null;
-			ServiceRegistry serviceRegistry = null;
-			if (resource == null) {
-				String exceptionMessage = "Resource can not be null";
+	private static SessionFactory buildSessionFactory(String resource) throws ArException {
+		Configuration configuration = null;
+		if (resource == null) {
+			String exceptionMessage = "Resource can not be null";
+			throw new ArException(exceptionMessage, logger);
+		}
+		try {
+			configuration = new Configuration().configure(resource);
+			if (configuration == null) {
+				String exceptionMessage = "Configuration can not be null";
 				throw new ArException(exceptionMessage, logger);
 			}
-			 try {
-				configuration = new Configuration().configure(resource);
-	        	serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-	        	if (serviceRegistry == null) {
-					String exceptionMessage = "serviceRegistry can not be null";
-					throw new ArException(exceptionMessage, logger);
-				}
-			 }catch (ConfigurationException e) {
-				throw new ArException("Resource "+resource+" not found on the classpath");
-			}
-			return configuration.buildSessionFactory(serviceRegistry);
+		} catch (ConfigurationException e) {
+			throw new ArException("Resource " + resource + " not found on the classpath");
 		}
+		return configuration.buildSessionFactory();
+	}
 
 	public static void shutdown(String resource) throws ArException {
 		getSessionFactory(resource).close();
